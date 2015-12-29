da3ca19d10a30d548b7e9ad10e4b458f1e4e0140
Qingqi Shi <qingqishi@live.com>
2015-12-29 15:57:43 +0000

    Created the Prototype subfolder

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using ClangSharp;
-
+﻿
 namespace ExplanationGenerator
 {
     class Program
@@ -11,48 +9,7 @@ namespace ExplanationGenerator
         /// <param name="args">The command-line arguments.</param>
         public static void Main(string[] args)
         {
-            /* Create Index */
-            var index = new Index();
-
-            /* Clang Arguments */
-            string[] arg = new string[1] { "" };
-
-            /* Create Translation Units */
-            TranslationUnit tu = index.CreateTranslationUnit("src/main.c", arg, null);
-
-            /* Creation Success */
-            if (tu != null)
-            {
-                /* Get Translation Unit Cursor */
-                Cursor cursor = tu.Cursor;
-
-                /* Visit Children */
-                DumpAST(cursor, 0);
-
-                /* Dispose Translation Unit */
-                tu.Dispose();
-            }
-
-            /* Dispose Index */
-            index.Dispose();
-
-            Console.ReadLine();
+            PrototypeMain.Run(args);
         }
-
-        static void DumpAST(Cursor rootCursor, int indent)
-        {
-            Console.WriteLine(rootCursor.Kind + " : " + rootCursor.Spelling);
-
-            foreach (var child in rootCursor.Children)
-            {
-                for (int i = 1; i < indent; i++)
-                {
-                    Console.Write("| ");
-                }
-                Console.Write("|- ");
-                DumpAST(child, indent + 1);
-            }
-        }
-
     }
 }
